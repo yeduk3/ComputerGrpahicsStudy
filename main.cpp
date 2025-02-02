1e24d2c10fede4b27d5c699f95d35d66a51bf2ed
yeduk3 <yeduk33@gmail.com>
2025-02-02 20:42:38 +0900

    Scan Line Conversion - left / right side check

--- main.cpp
+++ main.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
 
 #include "point.hpp"
-#include "testalgorithms/brensenham.hpp"
-#include "testalgorithms/DDA.hpp"
+// #include "linedrawing/linedrawing_comparator.hpp" // linedrawing_comp::
 
 using namespace std;
 
 int main()
 {
-    point<int> start(4, 3), end(90, 49); // case 1(1 quad, not steep) pass
-    // point<int> start(4, 3), end(49, 90); // case 2(1 quad, steep) pass
-    // point<int> start(4, 90), end(49, 3); // case 3(2 quad, steep) pass
-    // point<int> start(4, 30), end(90, 9); // case 4(2 quad, not steep) pass
-    // point<int> end(4, 3), start(90, 49); // case 5(3 quad, not steep) pass
-    // point<int> end(4, 3), start(49, 90); // case 6(3 quad, steep) pass
-    // point<int> start(4, 90), end(49, 3); // case 7(4 quad, steep) pass
-    // point<int> start(4, 90), end(49, 70); // case 8(4 quad, not steep) pass
+    point<int> start(0, 0), end(10, 7);
 
-    brsh::run(start, end);
-    dda::run(start, end);
-
-    bool flag = false;
-    for (int y = 0; y < brsh::TEST_RANGE; y++)
+    int scan = 4;
+    float m = (end.x - start.x) / (float)(end.y - start.y);
+    float intersection = m * scan;
+    cout << intersection << endl;
+    for (int i = 0; i < 10; i++)
     {
-        for (int x = 0; x < brsh::TEST_RANGE; x++)
-        {
-            if (brsh::map[y][x] != dda::map[y][x])
-            {
-                cout << "Not Correct on x = " << x << ", y = " << y << endl;
-                flag = true;
-            }
-        }
+        if (i < intersection)
+            cout << i << " is left\n";
+        else
+            cout << i << " is right\n";
     }
-    if (flag == false)
-        cout << "Same" << endl;
     return 0;
 }
